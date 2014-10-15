ceef529f090b005d02daa702d3657a41fe1ce9c8
Justin Long <jwlong.angelusdomini@gmail.com>
2014-10-14 22:00:41 -0500

    began work on customizing the authentication; mostly just disabled features for development

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/console', as: 'rails_admin'
   root to: 'visitors#index'
   devise_for :users
-  #resources :users
+
+  resources :users
 end
